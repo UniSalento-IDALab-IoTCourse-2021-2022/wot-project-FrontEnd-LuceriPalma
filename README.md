b904b0bfc8f183d2dff8c33e581c0fa4ea5cea1d
mluceri <107075103+mluceri@users.noreply.github.com>
2023-02-08 21:49:47 +0100

    added frontend

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
+
 # wot-project-FrontEnd-LuceriPalma
+
+Project Road Condition Monitoring System based on Nordic Thingy:52
+
+
+The complete project is composed by:
+- FrontEnd (https://github.com/UniSalento-IDALab-IoTCourse-2021-2022/wot-project-FrontEnd-LuceriPalma)
+- BackEnd (https://github.com/UniSalento-IDALab-IoTCourse-2021-2022/wot-project-BackEnd-LuceriPalma)
+- presentation (https://github.com/UniSalento-IDALab-IoTCourse-2021-2022/wot-project-presentation-LuceriPalma)
+
+
+
+## Authors
+
+- [@mluceri ](https://www.github.com/mluceri)
+- [@cpalma-usal](https://www.github.com/cpalma-usal)
+
+
+## Installation and instruction
+
+In the development environment, a local machine was utilized, with the following needed services:
+- A web server (http-server, live-server, ‘express’ in node app, or apache web server) working on port 8080 on localhost. This project mainly uses live-server, a little development server with live reload capability.
+- A database (JSON Server, MongoDB, PostgreSQL), working on port 3000 on localhost. This project mainly uses json-server, a little development database (DB) server, with live reload capability to create a quick REST API for a DB stored on file in .json format (db.json file).
+- A node application serverApp.js with extra functionalities such as web socket, routing control, access control, and more. ServerApp.js works on port 3002 on localhost.
+
+
+This repository contains the frontend : html, css, js files in order to run the web application.
+
+
+### Setting and launch of the web-server
+
+Assuming node and npm installed.
+
+```bash
+  npm i -g http-server
+  http-server
+```
+
+The server will be listening on :
+  http://localhost:8080
+
+
+
+## Acknowledgements
+ - [Nordic Thingy js library](https://github.com/NordicPlayground/Nordic-Thingy52-Thingyjs)
+ - [Leaflet interactive map](https://leafletjs.com/)
+ - [json-server](https://github.com/typicode/json-server/)
---
+++ index_track.js
@@ -0,0 +1,100 @@
+// Thingy:52 Web Bluetooth Minimal Example
+// Copyright (C) 2019, Uri Shaked
+// Released under the MIT license
+
+var x = document.getElementById("demo");
+var pos;
+var lat = document.getElementById("lat");
+var lng = document.getElementById("lng");
+var heading = document.getElementById("heading");
+var time = document.getElementById("time");
+var dir = document.getElementById("dir");
+
+function getTimestampInSeconds () {
+  return Math.floor(Date.now() / 1000)
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 30000,
+  timeout: 27000
+};
+function error(err) {
+  console.error(`ERROR(${err.code}): ${err.message}`);
+}
+
+function startLocation() {
+  if (navigator.geolocation) {
+    pos = navigator.geolocation.watchPosition(showPosition, error, options);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function stopLocation() {
+  if (navigator.geolocation) {
+    if (pos) {
+      navigator.geolocation.clearWatch(pos);
+    }
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+
+function showPosition(position) {
+//  var dateFormat= new Date(position.timestamp);
+//  var timestamp = getTimestampInSeconds();
+var timestamp = Date.now();
+  console.log(timestamp);
+  var dateFormat= new Date(timestamp);
+
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude +
+  "<br>Heading: " + position.heading +
+    "<br>Timestamp: " + timestamp +
+    "<br>Date: " + dateFormat.getDate()+
+           "/"+(dateFormat.getMonth()+1)+
+           "/"+dateFormat.getFullYear()+
+           " "+dateFormat.getHours()+
+           ":"+dateFormat.getMinutes()+
+           ":"+dateFormat.getSeconds() + "<br>" +
+            "<br>lng: " + position.coords.longitude + ","+
+            "lat: " + position.coords.latitude + ","+
+            "time: " + timestamp + ","+
+            "dir: " + "61.8" + ","+
+            "heading:" + "62";
+
+lat.innerHTML = position.coords.latitude;
+lng.innerHTML = position.coords.longitude;
+//console.log(position.timestamp);
+
+time.innerHTML = timestamp;
+if (position.coords.heading != null)
+{ heading.innerHTML = position.coords.heading; }
+else { heading.innertHTML = "62"; }
+dir.innerHTML = "61.8";
+
+//// mi serve questo
+    //{"lng":18.3231221,"lat":39.9732783,"time":1672977529,"dir":61.8,"heading":62,"info":[{"key":"name", "value":"CAR1"},{"key":"temp", "value":20}]},
+
+  console.log(position);
+  getOriginalMapPosition(position);
+  console.log("Date: "+ dateFormat.getDate()+
+           "/"+(dateFormat.getMonth()+1)+
+           "/"+dateFormat.getFullYear()+
+           " "+dateFormat.getHours()+
+           ":"+dateFormat.getMinutes()+
+           ":"+dateFormat.getSeconds());
+// update map
+
+
+
+}
